2f8383df33e98159971263ac8ecf5aa169480c7e
wzk <1356422517@qq.com>
2023-03-08 10:18:26 +0800

    锁定本地启动服务ip

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { fileURLToPath, URL } from 'node:url';
-import importToCDN, { autoComplete } from 'vite-plugin-cdn-import';
+import { fileURLToPath, URL } from 'node:url'
+import importToCDN, { autoComplete } from 'vite-plugin-cdn-import'
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -31,9 +31,12 @@ export default defineConfig({
       ],
     }),
   ],
+  server: {
+    host: '127.0.0.1',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-});
+})
